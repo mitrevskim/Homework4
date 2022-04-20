407a8b6285356d6d1bb88c33ff1f18610c1b4b45
mitrevskim <mitrevskimile@gmail.com>
2022-04-20 19:39:28 +0200

    Homework

---
+++ src/com/company/Firstclass.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Firstclass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hi colleagues");
+    }
+}
+
